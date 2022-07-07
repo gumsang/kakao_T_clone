1978b4602ce2f3b60f32ceae339c376e5e35fe1e
eunsang.kim <gomuzom@naver.com>
2022-07-07 11:50:20 +0900

    carousel_slider 의존성 추가

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,46 +17,100 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _buildTop() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Column(
-          children: [
-            Icon(
-              Icons.local_taxi,
-              size: 40,
-            ),
-            Text('택시'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.local_taxi,
-              size: 40,
-            ),
-            Text('택시'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.local_taxi,
-              size: 40,
-            ),
-            Text('택시'),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.local_taxi,
-              size: 40,
-            ),
-            Text('택시'),
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi_outlined,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('블랙'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.motorcycle,
+                    size: 40,
+                  ),
+                  Text('바이크'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.car_rental,
+                    size: 40,
+                  ),
+                  Text('대리'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Opacity(
+                opacity: 0,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
