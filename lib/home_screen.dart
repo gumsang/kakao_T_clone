7bcc91744bf0e5542ef35f8c1e1c3d00290bbbb7
eunsang.kim <gomuzom@naver.com>
2022-07-07 12:14:25 +0900

    url이미지 오토플레이, 하단 공지사항 추가

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -142,6 +142,17 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _buildBottom() {
-    return Text('top');
+    final items = List.generate(10, (i) {
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 이것은 공지사항입니다.'),
+      );
+    });
+
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:kakao_t_clone/kakao_main_screen.dart';
 
 final dummyItems = [
-  'https://images.app.goo.gl/q4mHgcbwsNb23dg7A',
-  'https://images.app.goo.gl/AD4f55qoPvHZVeYd9',
-  'https://images.app.goo.gl/hkruwLaMJY1ZbRvm9',
+  'http://goldshell.co.kr/web/product/big/201909/0afaa8a4d640df8bec11e5863403c4e2.jpg',
+  'http://www.dorocy.co.kr/shopimages/dorocyshop/2030000004042.jpg?1642389149',
+  'http://m.e-sang.co.kr/web/product/big/20191115/233261bfc05202b353eaf20b64319639.jpg',
 ];
 
 void main() {
